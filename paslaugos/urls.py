5fe40a5f1f31df94d94270d005b2ac1a67ea96e2
imac <busuko@yahoo.com>
2023-03-10 17:04:02 +0200

    papildytas

--- paslaugos/urls.py
+++ paslaugos/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('automobilio_modeliai/', automobilio_modeliai, name='automobilio_modeliai'),
     path('uzsakymas/', uzsakymas, name='uzsakymas'),
     path('paslauga/', paslauga, name='paslauga'),
+    path('automobilio_duomenys/<int:automobilio_modelis_id>/', automobiliu_duomenys, name='automobiliu_duomenys')
 
 ]
--- paslaugos/views.py
+++ paslaugos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 
@@ -47,5 +47,8 @@ def paslauga(request):
     }
     return render(request, 'paslaugos/paslauga.html', context=context)
 
-
+def automobiliu_duomenys(request, automobilio_modelis_id):
+    automobilio_modelis = get_object_or_404(AutomobilioModelis, pk=automobilio_modelis_id)
+    automobiliai = automobilio_modelis.automobilis_set.all()
+    return render(request, 'paslaugos/automobilio_duomenys.html', {'automobiliai': automobiliai})
 
