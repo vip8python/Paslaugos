b41d5f52bf9837399703ddb1a055effdd79ad540
imac <busuko@yahoo.com>
2023-03-10 12:18:00 +0200

    pataisytas

--- paslaugos/urls.py
+++ paslaugos/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index')
+    # path('automobiliai/', automobiliai, name='automobiliai' )
 ]
--- paslaugos/views.py
+++ paslaugos/views.py
@@ -9,6 +9,14 @@ def index(request):
     num_paslauga = Paslauga.objects.all().count()
     num_eilute = UzsakymoEilute.objects.all().count()
 
+    # def authors(request):
+    #     authors = Automobiliai.objects.all()
+    #     context = {
+    #         'authors': automobiliai
+    #     }
+    #     print(authors)
+    #     return render(request, 'automobiliai.html', context=context)
+
     context = {
         'num_modelis': num_modelis,
         'num_automobilis': num_automobilis,
