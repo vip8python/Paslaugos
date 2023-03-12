5df9466a65ee13816d65cb7057ad319a4965f65b
Juozas leilionas <busuko@yahoo.com>
2023-03-12 10:03:08 +0200

    render pakoreguotas

--- paslaugos/urls.py
+++ paslaugos/urls.py
@@ -3,13 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    # path('', index_meniu, name='index'),
     path('automobiliai/', automobiliai, name='automobiliai'),
     path('automobilio_modeliai/', automobilio_modeliai, name='automobilio_modeliai'),
     path('uzsakymas/', uzsakymas, name='uzsakymas'),
     path('paslauga/', paslauga, name='paslauga'),
     path('automobilio_duomenys/<int:automobilis_id>/', automobilio_duomenys, name='automobiliu_duomenys'),
     path('saskaitos/', SaskaitosListView.as_view(), name='saskaitos'),
-    path('saskaitos/<int:pk>', SaskaitosDetailView.as_view(), name='saskaitos-detail')
-
-]
+    path('saskaitos/<int:pk>', SaskaitosDetailView.as_view(), name='saskaitos-detail'),
+    ]
--- paslaugos/views.py
+++ paslaugos/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import *
 from django.views import generic
 
-meniu = ['Prideti paslauga', 'Prideti uzsakyma', 'Prideti automobili']
-
-def index_meniu(request):
-    return render(request, 'paslaugos/index.html', {'meniu': meniu, 'title': 'pagrindinis puslapis}'})
 
 def index(request):
     num_modelis = AutomobilioModelis.objects.all().count()
@@ -14,51 +10,48 @@ def index(request):
     num_paslauga = Paslauga.objects.all()
     num_eilute = UzsakymoEilute.objects.all().count()
 
+    meniu = ['Prideti paslauga', 'Prideti uzsakyma', 'Prideti automobili']
 
     context = {
         'num_modelis': num_modelis,
         'num_automobilis': num_automobilis,
         'num_uzsakymas': num_uzsakymas,
         'num_paslauga': num_paslauga,
-        'num_eilute': num_eilute
+        'num_eilute': num_eilute,
+        'meniu': meniu
     }
     return render(request, 'paslaugos/index.html', context=context)
 
+
 def automobiliai(request):
     automobiliai = Automobilis.objects.all()
-    context = {
-        'automobiliai': automobiliai
-    }
-    return render(request, 'paslaugos/automobiliai.html', context=context)
+    return render(request, 'paslaugos/automobiliai.html', {'automobiliai': automobiliai})
+
 
 def automobilio_modeliai(request):
     automobilio_modeliai = AutomobilioModelis.objects.all()
-    context = {
-        'automobilio_modeliai': automobilio_modeliai
-    }
-    return render(request, 'paslaugos/automobiliomodelis.html', context=context)
+    return render(request, 'paslaugos/automobiliomodelis.html', {'automobilio_modeliai': automobilio_modeliai})
+
 
 def uzsakymas(request):
     uzsakymas = Uzsakymas.objects.all()
-    context = {
-        'uzsakymas': uzsakymas
-    }
-    return render(request, 'paslaugos/uzsakymas.html', context=context)
+    return render(request, 'paslaugos/uzsakymas.html', {'uzsakymas': uzsakymas})
+
 
 def paslauga(request):
     paslauga = Paslauga.objects.all()
-    context = {
-        'paslauga': paslauga
-    }
-    return render(request, 'paslaugos/paslauga.html', context=context)
+    return render(request, 'paslaugos/paslauga.html', {'paslauga': paslauga})
+
 
 def automobilio_duomenys(request, automobilis_id):
     automobilio_modelis = get_object_or_404(Automobilis, pk=automobilis_id)
     return render(request, 'paslaugos/automobilio_duomenys.html', {'automobilio_duomenys': automobilio_modelis})
 
+
 class SaskaitosListView(generic.ListView):
     model = Uzsakymas
     template_name = 'paslaugos/saskaitos_list.html'
+
 
 class SaskaitosDetailView(generic.DetailView):
     model = Uzsakymas
@@ -68,6 +61,3 @@ class SaskaitosDetailView(generic.DetailView):
         context = super(SaskaitosDetailView, self).get_context_data(**kwargs)
         context['uzsakymo_eilutes'] = UzsakymoEilute.objects.all()
         return context
-
-
-# class Add
