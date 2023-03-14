230572d2d7d7a7c469628ecc074b3d3b6b8eecd9
imac <busuko@yahoo.com>
2023-03-14 13:39:24 +0200

    admin pakeistas

--- paslaugos/admin.py
+++ paslaugos/admin.py
@@ -25,3 +25,6 @@ admin.site.register(Automobilis, AutomobilisAdmin)
 admin.site.register(Uzsakymas, UzsakymasAdmin)
 admin.site.register(UzsakymoEilute)
 admin.site.register(Paslauga, PaslaugaAdmin)
+
+admin.site.site_title = 'autoservisas'
+admin.site.site_header = 'Autoserviso admin puslapis'
